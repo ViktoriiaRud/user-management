234cf22e04a91ef84a43fd168284e5a95b6de23c
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-09-10 19:00:38 +0200

    bug-fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 
 
 const root = ReactDOM.createRoot(
@@ -12,14 +10,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-     <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
